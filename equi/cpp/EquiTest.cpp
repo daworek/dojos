89724fa0e87e0970cb2f1c18e4ab86b2086cd0ec
Dawid Worek <dawid.worek@outlook.com>
2020-01-19 10:08:18 +0200

    Equilibrium solution

--- equi/cpp/EquiTest.cpp
+++ equi/cpp/EquiTest.cpp
@@ -3,13 +3,57 @@
 int equi(int inputArr[], int arrayLen)
 {
     // Find equilibrium index of inputArr
-    return 0;
+
+    long leftSum = 0;
+    long rightSum = 0;
+
+    // Sum of right side of the input array
+    for (int i = 1; i < arrayLen; i++)
+    {
+        rightSum += inputArr[i];
+    }
+
+    // Look for Equilibrium index
+    for (int i = 0; i < arrayLen - 1; i++)
+    {
+        if (leftSum == rightSum)
+        {
+            return i;
+        }
+
+        leftSum += inputArr[i];
+        rightSum -= inputArr[i+1];
+    }
+    if (leftSum == rightSum)
+    {
+        return arrayLen - 1;
+    }
+
+    return -1;
 }
 
 TEST(EquiTest, EmptyArrayReturnsZero)
 {
     const int arrayLen = 0;
     int inputArr[arrayLen] = {};
+    int equiIndex = equi(inputArr, arrayLen);
+    int expectedEquiIndex = -1;
+    ASSERT_EQ(expectedEquiIndex, equiIndex);
+}
+
+TEST(EquiTest, Equi3)
+{
+    const int arrayLen = 5;
+    int inputArr[arrayLen] = {1, -3, 4, -2, 2};
+    int equiIndex = equi(inputArr, arrayLen);
+    int expectedEquiIndex = 3;
+    ASSERT_EQ(expectedEquiIndex, equiIndex);
+}
+
+TEST(EquiTest, EquiNotExist)
+{
+    const int arrayLen = 6;
+    int inputArr[arrayLen] = {1, -3, 4, -2, 2, 2};
     int equiIndex = equi(inputArr, arrayLen);
     int expectedEquiIndex = -1;
     ASSERT_EQ(expectedEquiIndex, equiIndex);
